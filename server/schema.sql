059dcc91b11ae1c15300684160fe8da907755e08
Ryan De Leon <ryandeleon0@gmail.com>
2022-03-16 16:55:59 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,31 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+-- CREATE TABLE messages (
+--   ID int NOT NULL,
+--   Input1 varchar(255),
+--   Input2 varchar(255),
+--   PRIMARY KEY (ID)
+-- );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE Users (
+  id INT AUTO_INCREMENT NOT NULL,
+  Username INT DEFAULT NULL,
+  Friends INT DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE Messages (
+  id INT AUTO_INCREMENT NOT NULL,
+  messageText VARCHAR(255) DEFAULT NULL,
+  Room VARCHAR(255) DEFAULT NULL,
+  FromUserID INT DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
 
 /*  Execute this file from the command line by typing:
